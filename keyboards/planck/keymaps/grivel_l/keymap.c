195ec408391fd4fa04475660add5fdb695c4f750
Léo <legrivel@student.le-101.fr>
2019-12-29 00:55:33 +0100

    Fix error on GUI

--- keyboards/planck/keymaps/grivel_l/keymap.c
+++ keyboards/planck/keymaps/grivel_l/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    KC_LCTL, _______,   KC_LALT, LOWER,   KC_GUI,  KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, _______,   KC_LALT, LOWER,   KC_LGUI,  KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,
     _______, _______, _______, _______, _______, _______, KC_DEL,  KC_HOME, KC_END,  KC_PGUP, KC_PGDOWN, _______,
     BL_DEC,  BL_INC,  _______, _______, _______, KC_PSCREEN, KC_LEFT, KC_DOWN, KC_UP, _______, _______,  _______,
-    KC_MUTE, KC_VOLU, KC_VOLD, _______, KC_GUI, KC_SPC, _______, _______, _______, _______, _______, _______
+    KC_MUTE, KC_VOLU, KC_VOLD, _______, KC_LGUI, KC_SPC, _______, _______, _______, _______, _______, _______
 ),
 
 /* Raise
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_BTN3,    KC_7,    KC_8,    KC_9, KC_A,    KC_B,    KC_EXCLAIM,    KC_AT,    KC_LPRN,    KC_RPRN, KC_EQUAL,
     KC_DEL,  KC_F1,   KC_4,   KC_5,   KC_6,   KC_C,   KC_D,   KC_HASH, KC_DLR,  KC_LBRC, KC_RBRC, KC_CIRC,
     KC_LSFT, KC_X,   KC_1,   KC_2,   KC_3,  KC_E,  KC_F,  KC_MINUS, KC_ASTR, KC_AMPR, KC_PERC, KC_BSLS,
-    KC_LCTL, _______, KC_LALT, KC_0, KC_GUI, KC_SPC, _______, _______, _______, _______, _______, _______
+    KC_LCTL, _______, KC_LALT, KC_0, KC_LGUI, KC_SPC, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
