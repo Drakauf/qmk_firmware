178b841145ff176a040bb7e7c31c0386d9a8b0b7
Léo <legrivel@student.le-101.fr>
2019-12-29 02:03:54 +0100

    Add options layer + fix little errors

--- keyboards/planck/keymaps/grivel_l/keymap.c
+++ keyboards/planck/keymaps/grivel_l/keymap.c
@@ -22,7 +22,7 @@ enum planck_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _OPTS
 };
 
 enum planck_keycodes {
@@ -34,6 +34,7 @@ enum custom_keycodes {
   TMUX_PREFIX
 };
 
+#define OPTS MO(_OPTS)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    KC_LCTL, _______,   KC_LALT, LOWER,   KC_LGUI,  KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, OPTS,    KC_LALT, LOWER,   KC_LGUI,  KC_SPC,  KC_ENT,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -71,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_grid(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,
     _______, _______, _______, _______, _______, _______, KC_DEL,  KC_HOME, KC_END,  KC_PGUP, KC_PGDOWN, _______,
-    BL_DEC,  BL_INC,  _______, _______, _______, KC_PSCREEN, KC_LEFT, KC_DOWN, KC_UP, _______, _______,  _______,
-    KC_MUTE, KC_VOLU, KC_VOLD, _______, KC_LGUI, KC_SPC, _______, _______, _______, _______, _______, _______
+    BL_DEC,  BL_INC,  _______, _______, _______, KC_PSCREEN, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,  _______,
+    KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_LGUI, KC_SPC, _______, _______, _______, _______, _______, _______
 ),
 
 /* Raise
@@ -105,11 +106,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______,  _______,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+[_OPTS] = LAYOUT_planck_grid(
+    _______, RESET,   _______,   _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, _______,  _______,   _______,  _______, _______, _______,  _______,  _______,  _______,  _______,
+    AU_TOG, _______,  _______,  _______,   _______,  _______,   _______,  _______, _______, _______, _______, _______,
+    _______, _______, MU_TOG, MU_MOD, _______, _______, _______, _______, _______,  _______, _______, _______
 )
 
 };
@@ -120,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _LOWER, _RAISE, 0);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -206,12 +207,12 @@ void dip_switch_update_user(uint8_t index, bool active) {
 #ifdef AUDIO_ENABLE
                 if (play_sound) { PLAY_SONG(plover_song); }
 #endif
-                layer_on(_ADJUST);
+                layer_on(_OPTS);
             } else {
 #ifdef AUDIO_ENABLE
                 if (play_sound) { PLAY_SONG(plover_gb_song); }
 #endif
-                layer_off(_ADJUST);
+                layer_off(_OPTS);
             }
 #ifdef AUDIO_ENABLE
             play_sound = true;
@@ -250,6 +251,7 @@ void matrix_scan_user(void) {
 
 bool music_mask_user(uint16_t keycode) {
   switch (keycode) {
+    case OPTS:
     case RAISE:
     case LOWER:
       return false;
